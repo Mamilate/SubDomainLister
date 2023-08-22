6275766ce64fcd5826b7e59fe8c93f5a4fc337b2
Emin Aydemir <muhammedemin2071@gmail.com>
2023-08-22 19:36:16 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+def make_requests(url):
+    try:
+        return requests.get(url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+target_input = input("enter your target website: ")
+
+with open("subdomainlist.txt","r") as subdomain_list:
+    for word in subdomain_list:
+        word = word.strip()
+        url = "http://" + word + "." + target_input
+        response = make_requests(url)
+        if response:
+            print("Found subdomain-->" + url)
